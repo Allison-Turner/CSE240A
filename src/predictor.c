5f5e6df8f9f89ccde36a0f110f876578de6d79af
Allison-Turner <hermione2259@gmail.com>
2021-12-13 17:21:32 -0800

    tournament kinda works i think?

--- src/predictor.c
+++ src/predictor.c
@@ -42,62 +42,114 @@ const unsigned weak_taken = 0x10;
 const unsigned weak_not_taken = 0x01;
 const unsigned strong_not_taken = 0x00;
 
+//size of unsigned int in bytes
+unsigned unsigned_size = sizeof(unsigned);
+//and in bits
+unsigned unsigned_bits = sizeof(unsigned) * 8;
 
 //Gshare: global with index sharing
 //Similar to gselect predictor, except the branch address and global history are combined by an XOR into a 2-bit prediction
 unsigned global_history;
 unsigned* branch_history_table;
 
-//size of unsigned int in bytes
-unsigned unsigned_size = sizeof(unsigned);
-//and in bits
-unsigned unsigned_bits = sizeof(unsigned) * 8;
-
 unsigned ghistory_mask;
 unsigned bht_entry_mask = 0x11;
 
 //Tournament uses simple BHT for history-independent branches and correlated predictor for branches that need history
 //chooser starts branches in simple BHT, move to correlated predictor once a mis-prediction threshold is crossed
+const unsigned SIMPLE_BHT = 0x0;
+const unsigned CORRELATED_PREDICTOR = 0x1;
+
+unsigned* chooser;
+unsigned* total_predictions;
+unsigned* mispredictions;
+unsigned chooser_entry_mask;
+
+unsigned* local_pattern_hist;
+unsigned* pattern_hist_predictor_state;
+unsigned local_pattern_history_mask;
 
 
 //------------------------------------//
 //        Predictor Functions         //
 //------------------------------------//
 
+void
+init_gshare()
+{
+  global_history = (0x0 << ghistoryBits);
+  ghistory_mask = ((0x0 << (unsigned_bits - 0x1)) | ((0x1 << ghistoryBits) - 0x1));
+
+  unsigned num_bht_entries = (0x1 << ghistoryBits);
+  unsigned bht_size = (num_bht_entries * unsigned_size);
+
+  branch_history_table = (unsigned*)malloc(bht_size);
+
+  int iterator;
+  for (iterator = 0; iterator < num_bht_entries; iterator++){
+    branch_history_table[iterator] = weak_not_taken;
+  }
+}
+
+void
+init_tournament(){
+  
+  //init chooser
+  unsigned num_entries = (0x1 << pcIndexBits);
+  unsigned chooser_size = (num_entries * unsigned_size);
+  
+  chooser = (unsigned*)malloc(chooser_size);
+  total_predictions = (unsigned*)malloc(chooser_size);
+  mispredictions = (unsigned*)malloc(chooser_size);
+      
+
+  int iterator;
+  for(iterator = 0; iterator < num_entries; iterator++){
+    chooser[iterator] = SIMPLE_BHT;
+    total_predictions[iterator] = 0;
+    mispredictions[iterator] = 0;
+  } 
+
+  chooser_entry_mask = ((0x0 << (unsigned_bits - 0x1)) | ((0x1 << pcIndexBits) - 0x1));
+
+  //init local predictor
+  local_pattern_history_mask = ((0x1 << lhistoryBits) - 0x1);
+
+  local_pattern_hist = (unsigned*)malloc(chooser_size);
+  for(iterator = 0; iterator < num_entries; iterator++){
+    local_pattern_hist[iterator] = (0x0 << lhistoryBits);
+  }  
+
+  unsigned pattern_hist_predictors = (0x1 << lhistoryBits);
+  unsigned pht_size = (pattern_hist_predictors * unsigned_size);
+  
+  pattern_hist_predictor_state = (unsigned*)malloc(pht_size);
+
+  for(iterator = 0; iterator < pattern_hist_predictors; iterator++){
+    pattern_hist_predictor_state[iterator] = weak_not_taken;
+  }  
+
+  //init global predictor
+  init_gshare();
+}
+
 // Initialize the predictor
 //
 void
 init_predictor()
 {
+
   //
   //TODO: Initialize Branch Predictor Data Structures
   //
   switch (bpType){
 
     case GSHARE:
-      global_history = 0x0 << ghistoryBits;
-      ghistory_mask = (0x1 << ghistoryBits) - 0x1;
-
-      //printf("Global History Mask: %x\n", ghistory_mask);
-
-      unsigned num_bht_entries = 0x1 << ghistoryBits;
-
-      //printf("Num BHT Entries: %x\n", num_bht_entries);
-
-      unsigned bht_size = num_bht_entries * unsigned_size;
-
-      //printf("BHT Size: %x\n", bht_size);
-
-      branch_history_table = (unsigned*)malloc(bht_size);
-
-      int iterator;
-      for (iterator = 0; iterator < num_bht_entries; iterator++){
-        //printf("Iterator: %i\n", iterator);
-        branch_history_table[iterator] = weak_not_taken;
-      }
+      init_gshare();
       break;
 
     case TOURNAMENT:
+      init_tournament();
       break;
 
     //custom TBD
@@ -109,26 +161,72 @@ init_predictor()
   }
 }
 
+uint8_t
+predictor_state_to_binary_prediction(unsigned predictor_state){
+  if((predictor_state == strong_not_taken) || (predictor_state == weak_not_taken)){
+    return NOTTAKEN;
+  }
+  else if ((predictor_state == strong_taken) || (predictor_state == weak_taken)){
+    return TAKEN;
+  }
+}
 
+unsigned
+update_predictor_state(uint8_t outcome, unsigned predictor_state){
+  if(predictor_state == strong_not_taken){
+    if(outcome == TAKEN){
+      return weak_not_taken;
+    }
+    else{
+      return strong_not_taken;
+    }
+  }
+  else if(predictor_state == weak_not_taken){
+    if(outcome == TAKEN){
+      return weak_taken;
+    }
+    else{
+      return strong_not_taken;
+    }
+  }
+  else if(predictor_state == weak_taken){
+    if(outcome == TAKEN){
+      return strong_taken;
+    }
+    else{
+      return weak_taken;
+    }
+  }
+  else if(predictor_state == strong_taken){
+    if(outcome == TAKEN){
+      return strong_taken;
+    }
+    else{
+      return weak_taken;
+    }
+  }
+}
 
 uint8_t
 gshare_make_prediction(uint32_t pc)
 {
   unsigned xor = ghistory_mask & (pc ^ global_history);
-  unsigned prediction = branch_history_table[xor] & bht_entry_mask;
-
-  if ((prediction & strong_taken) || (prediction & weak_taken)){
-    return TAKEN;
-  }
-  else{
-    return NOTTAKEN;
-  }
+  return predictor_state_to_binary_prediction(branch_history_table[xor] & bht_entry_mask);
 }
 
 uint8_t
 tournament_make_prediction(uint32_t pc)
 {
-  return NOTTAKEN;
+  unsigned chooser_entry = pc & chooser_entry_mask;
+
+  if(chooser[chooser_entry] == SIMPLE_BHT){
+    unsigned local_history_pattern = local_pattern_hist[chooser_entry];
+    
+    return predictor_state_to_binary_prediction(pattern_hist_predictor_state[local_history_pattern]);
+  }
+  else{
+    return gshare_make_prediction(pc);
+  }
 }
 
 uint8_t
@@ -149,23 +247,23 @@ custom_make_prediction(uint32_t pc)
 uint8_t
 make_prediction(uint32_t pc)
 {
-  //
-  //TODO: Implement prediction scheme
-  //
-
   // Make a prediction based on the bpType
   switch (bpType) {
     case STATIC:
       return TAKEN;
+      break;
 
     case GSHARE:
       return gshare_make_prediction(pc);
+      break;
 
     case TOURNAMENT:
       return tournament_make_prediction(pc);
+      break;
 
     case CUSTOM:
       return custom_make_prediction(pc);
+      break;
 
     default:
       break;
@@ -180,62 +278,66 @@ make_prediction(uint32_t pc)
 void
 gshare_train_predictor(uint32_t pc, uint8_t outcome){
   unsigned bht_entry_index = ghistory_mask & (pc ^ global_history);
-  uint16_t prediction = branch_history_table[bht_entry_index] & bht_entry_mask;
+  uint16_t predictor_state = branch_history_table[bht_entry_index] & bht_entry_mask;
+
+  branch_history_table[bht_entry_index] = update_predictor_state(outcome, predictor_state);
+
+  //cut off first bit of global hist
+  unsigned temp = ghistory_mask & (global_history << 1);
 
   //NOTTAKEN
   if(outcome == 0){
-    
-    if(prediction == weak_not_taken){
-      branch_history_table[bht_entry_index] = strong_not_taken;
-    }
-    else if (prediction == weak_taken)
-    {
-      branch_history_table[bht_entry_index] = weak_not_taken;
-    }
-    else if (prediction == strong_taken){
-      branch_history_table[bht_entry_index] = weak_taken;
-    }
-
-    //cut off first bit of global hist
-    unsigned temp = ghistory_mask & (global_history << 1);
-
     //make sure last bit is 0
-    temp = temp & 0x0;
-    //printf("global history: %x\n", temp);
-
-    //set global hist to new val
-    global_history = temp;
-
+    temp = temp & ((0x1 << ghistoryBits) - 0x10);
   }
   //TAKEN
   else{
-    
-    if(prediction == strong_not_taken){
-      branch_history_table[bht_entry_index] = weak_not_taken;
-    }
-    else if (prediction == weak_not_taken)
-    {
-      branch_history_table[bht_entry_index] = weak_taken;
-    }
-    else if (prediction == weak_taken){
-      branch_history_table[bht_entry_index] = strong_taken;
-    }
-
-    //cut off first bit of global hist
-    unsigned temp = ghistory_mask & (global_history << 1);
-    
     //make sure last bit is 1
-    temp = temp | 0x1;
-    //printf("global history: %x\n", temp);
-
-    //set global hist to new val
-    global_history = temp;
+    temp = temp + 0x1;
   }
+
+  //set global hist to new val
+  global_history = temp;
 }
 
 void
 tournament_train_predictor(uint32_t pc, uint8_t outcome){
+  unsigned chooser_entry = pc & chooser_entry_mask;
 
+  unsigned local_history_pattern = local_pattern_hist[chooser_entry];
+  unsigned local_predictor_state = pattern_hist_predictor_state[local_history_pattern];
+  unsigned local_prediction = predictor_state_to_binary_prediction(local_predictor_state);
+
+  unsigned xor = ghistory_mask & (pc ^ global_history);
+  unsigned global_predictor_state = branch_history_table[xor] & bht_entry_mask;
+  unsigned global_prediction = predictor_state_to_binary_prediction(global_predictor_state);
+
+  total_predictions[chooser_entry] += 1;
+
+
+  //Track mispredictions and switch predictors if threshold passed
+  if(chooser[chooser_entry] == SIMPLE_BHT){
+    if(local_prediction != outcome){
+      mispredictions[chooser_entry] += 1;
+    }
+
+    //branch prediction algorithm chooser threshold but with cheating for unsigned ints
+    if((((double)mispredictions[chooser_entry]) / ((double)total_predictions[chooser_entry])) > 0.12){
+      chooser[chooser_entry] = CORRELATED_PREDICTOR;
+    }
+  }
+  else{
+    if(global_prediction != outcome){
+      mispredictions[chooser_entry] += 1;
+    }
+  }
+
+  //update local predictor
+  pattern_hist_predictor_state[local_history_pattern] = update_predictor_state(outcome, local_predictor_state);
+  local_pattern_hist[chooser_entry] = ((local_history_pattern << 1) | (outcome >> lhistoryBits)) & local_pattern_history_mask;
+
+  //update global predictor
+  gshare_train_predictor(pc, outcome);
 }
 
 void
@@ -256,12 +358,15 @@ train_predictor(uint32_t pc, uint8_t outcome)
   switch (bpType) {
     case GSHARE:
       gshare_train_predictor(pc, outcome);
+      break;
 
     case TOURNAMENT:
       tournament_train_predictor(pc, outcome);
+      break;
 
     case CUSTOM:
       custom_train_predictor(pc, outcome);
+      break;
 
     default:
       break;
@@ -276,7 +381,12 @@ cleanup(){
       break;
 
     case TOURNAMENT:
-      
+      free(chooser);
+      free(total_predictions);
+      free(mispredictions);
+      free(local_pattern_hist);
+      free(pattern_hist_predictor_state);
+      free(branch_history_table);
       break;
 
     case CUSTOM:
